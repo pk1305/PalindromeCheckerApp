d2cfb00267d1a9e45da1a3b1baed2a5f03115ca5
pk1305 <pk9955@srmist.edu.in>
2026-03-03 14:49:54 +0530

    UC9 Implementation

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,32 +1,26 @@
-
-import java.util.LinkedList;
 import java.util.Scanner;
 
-public class UC8 {
+public class UC9 {
+
+    public static boolean isPalindrome(String str, int start, int end) {
+
+        if (start >= end)
+            return true;
+
+        if (str.charAt(start) != str.charAt(end))
+            return false;
+
+        return isPalindrome(str, start + 1, end - 1);
+    }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        LinkedList<Character> list = new LinkedList<>();
 
         System.out.print("Enter a string: ");
         String str = sc.nextLine().toLowerCase();
 
-        // Add characters to linked list
-        for (char ch : str.toCharArray()) {
-            list.add(ch);
-        }
-
-        boolean isPalindrome = true;
-
-        while (list.size() > 1) {
-            if (!list.removeFirst().equals(list.removeLast())) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        if (isPalindrome)
+        if (isPalindrome(str, 0, str.length() - 1))
             System.out.println("The string is a Palindrome.");
         else
             System.out.println("The string is NOT a Palindrome.");
