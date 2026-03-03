b41eca3e954379af56cf8130bdd4f7c0128c6bc5
pk1305 <pk9955@srmist.edu.in>
2026-03-03 14:52:44 +0530

    Pooja Added UC10

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,26 +1,32 @@
+
 import java.util.Scanner;
 
-public class UC9 {
-
-    public static boolean isPalindrome(String str, int start, int end) {
-
-        if (start >= end)
-            return true;
-
-        if (str.charAt(start) != str.charAt(end))
-            return false;
-
-        return isPalindrome(str, start + 1, end - 1);
-    }
+public class UC10 {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a string: ");
-        String str = sc.nextLine().toLowerCase();
+        String str = sc.nextLine();
 
-        if (isPalindrome(str, 0, str.length() - 1))
+        // Remove spaces and convert to lowercase
+        str = str.replaceAll("\\s+", "").toLowerCase();
+
+        boolean isPalindrome = true;
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        if (isPalindrome)
             System.out.println("The string is a Palindrome.");
         else
             System.out.println("The string is NOT a Palindrome.");
